8ee0c85b8bea1fb516f2f2642deacd45a4a35086
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-13 19:57:14 +0530

    add alert box in the place of div

--- scripts/script.js
+++ scripts/script.js
@@ -9,27 +9,43 @@ btnAddTask.addEventListener('click', function (e) {
   e.preventDefault();
 
   const now = new Date();
+
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }
   if (inputTask.value) {
+
+
     //Create div
-    const div = document.createElement('div');
-    div.innerHTML = `<div class="container text-center" id="user__task-list">
-    <p>${Intl.DateTimeFormat('en-IN').format(now)}</p>
-      <h5>${inputTask.value}</h5>
-    <i class="fa-solid fa-square-xmark icon__close"></i>
+    const taskDiv = document.createElement('div');
+    taskDiv.innerHTML = `<div class="alert alert-info d-flex justify-content-between" role="alert">
+    <p class="fs-5">${Intl.DateTimeFormat('en-IN').format(now)}</p> 
+    <p class="fs-5">${Intl.DateTimeFormat('en-IN',options).format(now)}</p>
+    <h4 class ="text-primary">${inputTask.value}
+    </h4>
+    <button type="button" class="btn-close fs-5" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    `;
+
+    // Add task
+    app.append(taskDiv);
+
+    //clear all task
+    btnClear.addEventListener('click', function () {
+      taskDiv.remove();
+    });
+
+    inputTask.value = '';
+  } else {
+    const alertDiv = document.createElement('div');
+    alertDiv.innerHTML = `<div class="alert alert-danger" role="alert">
+   Please enter your task first!
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
   </div>`;
 
-  // Add task 
-    app.append(div);
-
-
-      //clear all task
-      btnClear.addEventListener('click',function(){
-        div.remove();
-      })
-      
-        inputTask.value = '';
-  }else{
-    const alertMessage = document.createElement('p').textContent = `Please enter task`
-    app.prepend(alertMessage)
+    app.prepend(alertDiv);
   }
 });
