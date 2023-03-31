00c2627f7846be9a719396fa04be57e5cbf73fc8
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-31 19:32:22 +0530

    add enter funtionalty

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,72 @@
 'use strict';
 
 const inputTask = document.querySelector('.input__task');
+const containerApp = document.getElementById('application');
 const btnAddTask = document.querySelector('.btn__add-task');
-const app = document.getElementById('application');
 const btnClear = document.querySelector('.btn__clear');
+const form = document.querySelector('#input__form');
 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function(){
-  app.classList.remove('hidden');
-},function(){
-  alert('Allow the location')
-}) 
-
-btnAddTask.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  const now = new Date();
-
-  const options = {
-    hour: 'numeric',
-    minute: 'numeric',
-  };
-  if (inputTask.value) {
-    //Create div
-    const taskDiv = document.createElement('div');
-    taskDiv.style.backgroundColor = '#191825';
-    taskDiv.innerHTML = `<div class="alert  d-flex justify-content-between task" role="alert">
-    <p class="fs-5">${Intl.DateTimeFormat('en-IN').format(now)}</p> 
-    <p class="fs-5">${Intl.DateTimeFormat('en-IN', options).format(now)}</p>
-    <p class ="fs-5">${inputTask.value}
-    </p>
-    <button type="button" class="btn-close fs-6" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    `;
-
-    // Add task
-    app.append(taskDiv);
-
-    //clear all task
-    btnClear.addEventListener('click', function () {
-      taskDiv.remove();
+class App {
+  #task = [];
+  constructor() {
+    //Get Location For No Resone 😁
+    navigator.geolocation.getCurrentPosition(this._showApp, function () {
+      alert('Allow the location');
     });
 
-    inputTask.value = '';
-  } else {
-    const messageAlert = document.createElement('div');
-    messageAlert.innerHTML = `<div class="alert alert-danger text-center" role="alert">
-   Please enter your task first!
-    <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>
-  </div>`;
+    // Add User div
+    btnAddTask.addEventListener('click', this._addTask);
 
-    app.prepend(messageAlert);
+    // Cleat All User Task List
+
+    // Add User Task with KeyEnter
+    form.addEventListener('submit', this._addTask);
   }
-});
+
+  _showApp() {
+    containerApp.classList.remove('hidden');
+  }
+
+  _addTask(e) {
+    e.preventDefault();
+    const now = new Date();
+    let div;
+
+    if (inputTask.value) {
+      //Add A div
+      div = document.createElement('div');
+      div.style.backgroundColor = '#191825';
+      div.classList.add('list');
+      div.innerHTML = `<div class="alert d-flex justify-content-between task" role="alert">
+      <p class="fs-5">${Intl.DateTimeFormat('en-IN',{year:"2-digit",month:"short",day:"numeric"}).format(now)}</p> 
+      <p class="fs-5">${Intl.DateTimeFormat('en-IN', {
+        hour: 'numeric',
+        minute: 'numeric',
+      }).format(now)}</p>
+      <p class ="fs-5">${inputTask.value}
+      </p>
+      <button type="button" class="btn-close fs-6" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    `;
+
+      containerApp.append(div);
+      inputTask.value = '';
+
+      btnClear.addEventListener('click', function () {
+
+        div.remove();
+      });
+    } else {
+      const messageAlert = document.createElement('div');
+      messageAlert.innerHTML = `<div class="alert alert-danger text-center" role="alert">
+       Please enter your task first!
+        <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+      </div>`;
+
+      containerApp.prepend(messageAlert);
+    }
+  }
+}
+
+const app = new App();
