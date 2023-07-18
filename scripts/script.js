96ef7e1c1fd3374f60187f30762fd8fa57ea7742
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-07-18 11:58:23 +0530

    refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,87 +1,70 @@
 'use strict';
 
+const containerApp = document.querySelector('#app');
 const inputTask = document.querySelector('.input__task');
-const containerApp = document.getElementById('application');
 const btnAddTask = document.querySelector('.btn__add-task');
 const btnClear = document.querySelector('.btn__clear');
 const form = document.querySelector('#input__form');
 const taskContainer = document.getElementById('task__list');
 
 class Task {
-  constructor(task) {
+  constructor(task, date) {
     this.task = task;
+    this.date = date;
   }
 }
 
 class App {
+  #date;
   #task = [];
+  #list;
   constructor() {
-    //Get Location For No Resone 😁
-    navigator.geolocation.getCurrentPosition(this._showApp, function () {
-      alert('Allow the location');
-    });
+    //Render task
+    form.addEventListener('submit', this.#addTask.bind(this));
 
-    // Add User div
-    btnAddTask.addEventListener('click', this._addTask.bind(this));
-
-    // Add User Task with KeyEnter
-    form.addEventListener('submit', this._addTask.bind(this));
-
-    // Get data form local storage
+    // load data form local storage
     this._getLocalStorage();
 
     //clear all tasks
     btnClear.addEventListener('click', this._clearAllTask);
-
-    //Remove single task
-    this._removeSingleTask();
-  }
-
-  _showApp() {
-    containerApp.classList.remove('hidden');
   }
 
   _clearAllTask() {
-    taskContainer.classList.add('hidden');
+    taskContainer.innerHTML = '';
     localStorage.clear();
   }
-  _addTask(e) {
+
+  #renderTask(task) {
+    const markup = `
+    <div class="alert alert-dismissible" id="list">
+    <p>${task.date}</p>
+    ${task.task}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
+    taskContainer.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  #addTask(e) {
     e.preventDefault();
     const task = inputTask.value;
-    let list;
+    if (!task) return;
 
-    if (task) {
-      //Add A div
-      const html = `<div class="list">
-		  <div class="alert d-flex justify-content-between task" role="alert">
-      <p class ="fs-5">${task}
-      </p>
-      <button type="button" class="btn-close fs-4" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    	</div>`;
+    this.#createDate(new Date());
 
-      taskContainer.insertAdjacentHTML('afterbegin', html);
+    // create object with Task class
+    this.#list = new Task(task, this.#date);
 
-      inputTask.value = '';
+    //render task
+    this.#renderTask(this.#list);
 
-      // create object with Task class
-      list = new Task(task);
+    // push list object in task array
+    this.#task.push(this.#list);
 
-      // push list object in task array
-      this.#task.push(list);
+    //clear input field
+    this.#clearInput();
 
-      // Store data in local storage
-      this._setLocalStorage();
-    } else {
-      const messageAlert = document.createElement('div');
-      messageAlert.innerHTML = `<div class="alert alert-danger text-center" role="alert">
-      Please enter your task first!
-      <button type="button" class="btn-close float-end" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-      </div>`;
-
-      containerApp.prepend(messageAlert);
-    }
+    // Store data in local storage
+    this._setLocalStorage();
   }
 
   _setLocalStorage() {
@@ -90,34 +73,24 @@ class App {
 
   _getLocalStorage() {
     const data = JSON.parse(localStorage.getItem('task'));
-
     if (!data) return;
-
     this.#task = data;
-
-    this.#task.forEach(task => this._renderTask(task));
-
-    // this.#task.forEach(sTast => this._removeSingleTask(sTast));
+    this.#task.forEach(task => this.#renderTask(task));
   }
 
-  _renderTask(task) {
-    const html = `<div class="list">
-		  <div class="alert d-flex justify-content-between task" role="alert">
-      <p class ="fs-5">${task.task}
-      </p>
-      <button type="button" class="btn-close fs-4" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    	</div>`;
-
-    taskContainer.insertAdjacentHTML('afterbegin', html);
+  #clearInput() {
+    inputTask.value = '';
   }
-  // _removeSingleTask(task) {
-  //   taskContainer.addEventListener('click', function (e) {
-  //     const singleTask = e.target;
 
-  //     console.log(task);
-  //   });
-  // }
+  #createDate(date) {
+    this.#date = Intl.DateTimeFormat(navigator.language, {
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    }).format(date);
+  }
 }
 
 const app = new App();
